5da5c36a382ee49b026a13922a111f9d31f316c3
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-04-02 12:43:33 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 24004 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int dp[] = new int[N + 1];
+        int arr[] = new int[N + 1];
+        int max_dp = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[1] = arr[1];
+        max_dp = dp[1];
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+
+            max_dp = Math.max(max_dp, dp[i]);
+        }
+
+        System.out.println(max_dp);
+        br.close();
+    }
+}
