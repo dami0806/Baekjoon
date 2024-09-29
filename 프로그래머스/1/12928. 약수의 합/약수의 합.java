ed005e94e7252c4c61b97ad4550c689e9220d559
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-09-29 12:15:34 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int n) {
         //n으로 나누어 떨어지는것들 합
         int answer = 0;
-        for(int i = 1; i<=n; i++) {
+        for(int i = 1; i<=n/2; i++) {
             if(n%i == 0) {
                 answer += i;
             }
         }
-        return answer;
+        return answer+n;
     }
 }
