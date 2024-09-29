2922bf20af1d89c88077e470d255752353e21a48
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-09-29 14:48:04 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        //자릿수 숫자 구하기 n이 몇자리수인지 어떻게 알지?m자릿수?
-        int m=10;
-        int answer = 0;
-
-        while(m<=n) {
-            m*=10;
-        }
-        m/=10;
-        while(m>=1){
-         answer+= (n/m);
-          n%=m;
-          m/=10;
+        int answer =0;
+        
+        while(true) {
+            answer+=n%10;
+            if(n<10) break;
+            n/=10;
         }
     
-    return answer;
+        return answer;
     }
 }
