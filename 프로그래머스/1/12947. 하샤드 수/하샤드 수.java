80c8f77b8f8ee944de6370a7da78f460070ccae7
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-10-01 16:47:24 +0900

    [level 1] Title: 하샤드 수, Time: 2.43 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean solution(int x) {
         int sum = String.valueOf(x).chars().map(Character::getNumericValue).sum();
-        if(x%sum == 0)return true;
-        return false;
+        return x%sum == 0;
     }
 }
