654b5b70e965d5e9877d3cdd54527452b7ba8d0a
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-10-02 16:07:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 999.25 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,24 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr) {
-              if (arr.length == 1) {
-            return new int[] {-1};  // 배열에 요소가 하나만 있을 경우
-        }
-        // 가장 작은 수 찾기
-        int min = Arrays.stream(arr).min().orElseThrow();
+         return  arr.length == 1 ? new int[]{-1} :
+                (Arrays.stream(arr)
+                        .filter(i -> i != Arrays.stream(arr).min().orElseThrow()).toArray());
 
-        // 가장 작은 수를 제거한 배열 만들기
-        int[] result = new int[arr.length - 1];
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != min) {
-                result[index++] = arr[i];
-            }
-        }
-        return result;
+    
     }
 }
