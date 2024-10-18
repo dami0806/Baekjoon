d4abf651a4e51dd6f7710d03c79dae1563732e21
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-10-19 00:31:07 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.80 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,27 +1,29 @@
-import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr) {
+        if(arr.length == 1) return new int[]{-1};
         
-        if (arr.length == 1) {
-            return new int[]{-1};
+        return noMin(arr);
+    }
+    // 가장 작은 수 찾기
+    public int findMin(int[]arr){
+        int min = arr[0];
+        for(int i = 1; i<arr.length; i++) {
+           min = min>arr[i] ? arr[i]: min;
         }
-
-        int min = Integer.MAX_VALUE;
-
-        // 가장 작은수 찾기
-        for (int i = 0; i < arr.length; i++) {
-            min = Math.min(min, arr[i]);
-        }
-
-        // 가장 작은수 제거
-        int[] result = new int[arr.length - 1];
+        return min;
+    }
+    // 가장 작은수 없이 추가
+    public int[] noMin(int[]arr) {
+        int[] newarr = new int[arr.length-1];
+         int min = findMin(arr);
         int idx = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != min) {
-                result[idx++] += arr[i];
+        
+        for(int i =0; i<arr.length; i++){
+            if(arr[i] != min) {
+                newarr[idx] = arr[i];
+                idx++;
             }
         }
-        return result;
+        return newarr;
     }
 }
