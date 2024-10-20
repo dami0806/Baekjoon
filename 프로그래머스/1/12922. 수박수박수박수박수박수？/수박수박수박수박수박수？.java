9305b23f7e84a89900f6910b1a74c1a8c9b6ffac
dami0806 <85047035+dami0806@users.noreply.github.com>
2024-10-21 01:37:56 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.86 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(int n) {
-        String answer = "";
-       for(int i = 0; i<n/2; i++) {
-           answer += "수박"; 
-       }
-        if(n%2==1) answer +="수";
-        return answer;
+        String st = "수박";
+        st = st.repeat(n/2);
+        
+        if(n%2==1) st += "수";
+        return st;
     }
 }
