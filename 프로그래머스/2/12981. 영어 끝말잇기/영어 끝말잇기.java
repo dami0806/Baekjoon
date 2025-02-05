fdb7e22df56a8dce2b14236dda16bbf5bea6a037
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-02-06 01:51:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0,0};
+        int arr = 0;
+
+        for(int i = 0; i<words.length-1; i++) {
+             
+                if((!(words[i].charAt(words[i].length()-1)
+                     == (words[i+1].charAt(0))
+                    )) || (isSame(words, i+1)) ) {
+                    answer[0] = ((i+1)%n)+1;
+                    answer[1] = ((i+1)/n)+1;
+                    // System.out.println(i);
+                    // System.out.println(words[i+1]);
+                    break;
+                }
+        }
+        
+        return answer;
+    }
+    public boolean isSame(String[] words, int index) {
+        // 중복
+        for(int i = 0 ;i<index; i++) {
+            if(words[i].equals(words[index])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
