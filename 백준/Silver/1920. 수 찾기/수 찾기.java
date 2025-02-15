8feed3e85a26b900a5032e45b419e02cca271191
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-02-16 01:55:56 +0900

    [Silver IV] Title: 수 찾기, Time: 1836 ms, Memory: 92848 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,57 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
-/*
-5
-4 1 5 2 3
-5
-1 3 7 9 5
+/**
+ * 5
+ * 4 1 5 2 3
+ * 5
+ * 1 3 7 9 5
  */
 public class Main {
 
+    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        // 첫 번째 줄: N 입력
-        int n = Integer.parseInt(br.readLine());
-
-        // 두 번째 줄: N개의 정수 배열 A 입력
-        int[] arrayA = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        // 배열을 오름차순으로 정렬 (이분 탐색을 위해)
-        Arrays.sort((arrayA));
-
-        // 세 번째 줄: M 입력
-        int m = Integer.parseInt(br.readLine());
-
-        // 네 번째 줄: M개의 수들이 주어짐, 각 수에 대해 존재 여부 판단
-        int[] arrayB = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int first[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            first[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(first);
 
 
-        for (int query : arrayB) {
-            System.out.println(validate(arrayA, query) ? 1 : 0);
+        M = Integer.parseInt(br.readLine());
+        int second[] = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            second[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (Binary_search(first, second[i])) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+
         }
     }
 
-    public static boolean validate(int[] arrayA, int query) {
-        int left = 0;
-        int right = arrayA.length - 1;
+    private static boolean Binary_search(int[] first, int target) {
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        int start = 0;
+        int end = N-1;
 
-            if (arrayA[mid] == query) {
-                return true;
-            } else if (arrayA[mid] < query) {
-                left = mid + 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (first[mid] > target) {
+                end = mid - 1;
+            } else if (first[mid] < target) {
+                start = mid + 1;
             } else {
-                right = mid - 1;
+                return true;
             }
-
         }
         return false;
     }
