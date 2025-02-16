86e9c955f80fd3e4729d89873fdbb47d651ab140
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-02-16 19:39:33 +0900

    [Silver V] Title: 수들의 합 5, Time: 100 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,37 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.sql.Array;
-import java.util.*;
 
-
-/**
- * 15, 7+8, 4+5+6, 1+2+3+4+5의 4가지
- */
 public class Main {
-
     public static void main(String[] args) throws IOException {
-         Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int count = 1;
-    int start_index = 1;
-    int end_index = 1;
-    int sum = 1;
-    while (end_index != N) {
-      if (sum == N) {         //sum == N ->  End index++;  sum = sum + End index;  count++;  
-        count++;
-        end_index++;
-        sum = sum + end_index;
-      } else if (sum > N) {   //sum > N ->  sum = sum - Start index;  Start index++;
-        sum = sum - start_index;
-        start_index++;
-      } else {                //sum < N ->  End index++;  sum = sum + End index;
-        end_index++;
-        sum = sum + end_index;
-      }
-    }
-    System.out.println(count);
-  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int start = 1, end = 1;
+        int sum = 1, count = 0;
+
+        while (start <= n / 2) { // n/2까지만 탐색
+            if (sum < n) {
+                end++;
+                sum += end;
+            } else if (sum > n) {
+                sum -= start;
+                start++;
+            } else {
+                count++;
+                sum -= start;
+                start++;
+            }
+        }
+        System.out.println(count + 1); // 자기 자신을 포함해야 하므로 +1
     }
 }
