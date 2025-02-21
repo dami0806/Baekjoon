a1111ee753c3389a17877e8a1adc117ffe58c087
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-02-21 23:30:18 +0900

    [level 2] Title: 구명보트, Time: 11.92 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,30 +1,28 @@
 import java.util.*;
-
 class Solution {
+    
     public int solution(int[] people, int limit) {
        
-        Arrays.sort(people);
+        Arrays.sort(people);  //[40, 50, 80, 70]  40kg 이상이면 60넘는 사람은
         int count = 0;
-        int maxcount=people.length-1, mincount=0;   
-
-        while(maxcount >= mincount) {
-           int min = people[mincount], max= people[maxcount];
-
-           if (maxcount == mincount) {
-               count++;
-               break;
-           }
-            if (max+min > limit) {
-                count++; 
-                max = people[maxcount--];
-           }
-            else if (max+min <= limit) {
-                count++; 
-                max = people[maxcount--]; 
-                min = people[mincount++]; 
+        int start = 0;
+        int end = people.length-1;
+       // for(int i = 0; i< (int)Math.ceil(people.length/2); i++){
+        while(start<=end) {
+            
+            if(people[end]+people[start]<=limit) {
+  
+            start++;
+            count++;
+            end--;
             }
-        }
-                
+            else{
+               
+                end--;
+                count++;
+            }
+        }    
+                    
         return count;
     }
 }
