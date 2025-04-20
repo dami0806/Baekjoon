f001a3878ad7272e3ceac1cba5e3ee416a5cafac
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-04-20 22:39:20 +0900

    [D2] Title: 중간 평균값 구하기, Time: 72 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,37 +1,28 @@
+
 import java.util.*;
 import java.io.*;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+class Solution {
+	public static void main(String args[]) throws Exception {
 		
-		int T = Integer.parseInt(st.nextToken());
-		int answer = 0;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+    
 		for(int test_case = 1; test_case <= T; test_case++) {
-			 st = new StringTokenizer(br.readLine());
-            ArrayList<Integer> list = new ArrayList<>();
-            // 최대값 찾기, 최솟값 찾기
-            int min =10000 ,max=0;
-            for(int i = 0 ; i < 10; i++) {
-               int a = Integer.parseInt(st.nextToken());
-                min = Math.min(min, a);
-                max = Math.max(max, a);
-            	list.add(a); // 3 17 1 39 8 41 2 32 99 2 
-            }
-            list.remove(Integer.valueOf(min));
-            list.remove(Integer.valueOf(max));
+              int arr[] = new int[10];
+            int answer =0;
+             StringTokenizer st = new StringTokenizer(br.readLine());
             
-            int avg = 0;
-            
-            // list 평균 구하기
-            for(int i = 0 ; i < list.size(); i++){
-            	 avg += list.get(i);
-            }
-           answer =  (int)Math.round((double)avg / list.size());
-			System.out.println("#" + test_case + " " + answer);
+             for(int i =0;i< 10;i++){
+                 arr[i] = Integer.parseInt(st.nextToken());
+             }
+            Arrays.sort(arr);
+              for(int i =1;i< 9;i++){
+               	answer+=arr[i];
+              }
+            answer =(int)Math.round(answer/8.0);
+            System.out.println("#" + test_case+ " "+ answer);
 		}
 	}
 }
