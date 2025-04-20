c45cd1e95d49224870533eaea8b7ce535240bc52
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-04-20 20:58:30 +0900

    [D2] Title: 간단한 369게임, Time: 112 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,37 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = &apos;b&apos;;
+// String var = &quot;ABCDEFG&quot;;
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
 
-import java.util.*;
-import java.io.*;
-
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream(&quot;res/input.txt&quot;));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
 		Scanner sc = new Scanner(System.in);
 		int T;
-		T=sc.nextInt();
-
-		for (int test_case = 1; test_case <= T; test_case++) // 10
-		{
-			String s = String.valueOf(test_case); //13
-            // 받아와서 string으로
-            int count = 0;
-            for (char c : s.toCharArray()){
-            	if ((c == '3') ||( c == '6') ||(c == '9')) {
-                	count++;
-                }
-            }
-            // 그 string을 char로 해서 3,6,9 검사, if -로, 아니면 그냥 숫자
-            // clap 숫자로 
-            
-                // clap이 0> 갯수 만큼 - 
-            if(count > 0) {
-                for(int i = 0 ; i<count; i++){
-            		System.out.print('-');
-                }System.out.print(" ");
-            }
-            else {
-            System.out.print(test_case + " ");
-            }
-		}
+		T=sc.nextInt(); //10
+        for(int i = 1 ; i <=T; i++) {
+            // i가 3 6 9 가 들어갔는지 35 는 - 36은--
+            // i -> String으로 변환
+            // String을 char로 쪼개서 검사 3의 갯수
+            // 0개일땐 &quot;숫자&quot; 그대로 리턴, 1개일때 - 2개일때 -- N개일때 -*N
+            //
+            String str = checkNum(i);
+       	    System.out.print(str+ " " );
+        }		
 	}
+    
+    static String checkNum(int num) {
+        String s = String.valueOf(num); // int -> string: 
+        int count = 0;
+        String answer = "";
+        
+        for(char c : s.toCharArray() ){
+        	if(c=='3' || c=='6' || c=='9') {
+                count++;
+            }
+        }
+
+        if(count == 0) {
+            answer = s;
+        }
+        else {
+        	StringBuilder sb = new StringBuilder();
+            for(int i = 0 ; i< count; i++){
+                sb.append("-");
+            }
+            answer = sb.toString();
+        }
+        return answer;
+    }
 }
