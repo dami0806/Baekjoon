5e69624a8b8c1da38e122cf4c2f672e881a009d2
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-04 19:17:07 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,23 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        long[][] dp = new long[n+1][2];
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 3
+        int N = Integer.parseInt(br.readLine().trim());
 
+        long dp[][] = new long[N+1][2];
+        dp[1][1]=  1;
         dp[1][0] = 0;
-        dp[1][1] = 1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+
+
+        for(int i = 2; i<=N; i++) {
+            dp[i][0] = dp[i-1][1] + dp[i-1][0];
             dp[i][1] = dp[i-1][0];
         }
-
-        System.out.println(dp[n][0] + dp[n][1]);
-        br.close();
+        System.out.println(dp[N][0] + dp[N][1]);
     }
 }
