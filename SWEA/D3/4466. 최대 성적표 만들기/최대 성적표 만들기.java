38150d4cd184c6b7365edae84f08c317f02e7b2e
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-15 20:15:51 +0900

    [D3] Title: 최대 성적표 만들기, Time: 101 ms, Memory: 28,160 KB -BaekjoonHub

---
+++ SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken()); // 3
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int arr[] = new int[N];
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			int sum = 0;
+			for (int i = 0;i<M;i++) {
+				sum += arr[N-i-1];
+			}
+
+			System.out.println("#" + test_case + " " + sum);
+		}
+	}
+}
