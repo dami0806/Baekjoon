1b59e2d390e118140c5477ec14385f70c3662944
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-16 13:03:03 +0900

    [D3] Title: 힙, Time: 482 ms, Memory: 100,008 KB -BaekjoonHub

--- SWEA/D3/2930. 힙/힙.java
+++ SWEA/D3/2930. 힙/힙.java
@@ -2,35 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
+	static int arr[];
+	static HashSet<Integer> set;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken()); // 3
 
 		for (int test_case = 1; test_case <= T; test_case++) {
+
 			PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 			int N = Integer.parseInt(br.readLine().trim());
 			StringBuilder sb = new StringBuilder();
-
+			String answer = " ";
+			
 			for (int i = 0; i < N; i++) {
-				String line = br.readLine();
-				if (line == null || line.isEmpty()) {
-					continue; // 입력 부족 방지
-				}
-				StringTokenizer st = new StringTokenizer(line);
-				String command = st.nextToken();
-
-				if (command.equals("1")) {
+				st = new StringTokenizer(br.readLine());
+				if (st.nextToken().equals("1")) {
 					int n = Integer.parseInt(st.nextToken());
 					pq.offer(n);
 				} else {
 					if (pq.isEmpty()) {
-						sb.append("-1 ");
-					} else {
-						sb.append(pq.poll()).append(" ");
-					}
+						sb.append(" ").append("-1");
+                    }else{
+					sb.append(" ").append(pq.poll());
+                    }
 				}
 			}
-			System.out.println("#" + test_case + " " + sb.toString().trim());
+			System.out.println("#" + test_case + " " +sb.toString().trim());
 		}
 	}
 }
