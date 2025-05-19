277690dd55a9181c8b8aada97771a0e7c46990b5
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-19 16:51:10 +0900

    [D3] Title: 과자 분배, Time: 163 ms, Memory: 33,644 KB -BaekjoonHub

---
+++ SWEA/D3/10032. 과자 분배/과자 분배.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			// 100 10    
+			
+			int answer = N%M ==0? 0: 1;
+
+
+
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+}
