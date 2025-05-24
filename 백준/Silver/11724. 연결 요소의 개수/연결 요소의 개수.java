78dc8a6a34313b15a962428549ee127f53a166ea
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-24 13:41:06 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 141908 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,64 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
-/**
- * 6 5
- * 1 2 ArrayList[]
- * 2 5
- * 5 1
- * 3 4
- * 4 6
- */
 public class Main {
-    static ArrayList<Integer>[] A; // 1 2 3 4 5 6
-    static boolean[] visited; // 1 false, 2 false 3...
-    static int count = 0;
+	static ArrayList<Integer> list[];
+	static boolean visited[];
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int answer = 0;
+		
+		list = new ArrayList[n+1];
+		visited = new boolean[n+1];
+		for (int i = 0; i < n+1; i++) {
+			list[i] = new ArrayList<>();
+		}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
+		}
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				bfs(i);
+				answer++;
+			}
+		}
+		System.out.println(answer);
 
-        A = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
+	}
 
-        for (int i = 1; i <= n; i++) {
-            A[i] = new ArrayList<Integer>();
-        }
-        for (int j = 1; j <= m; j++) {
-            // m개의 간선 5줄을 입력 받았음 1 2 일때 1->2 2->1
-            st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            A[v1].add(v2);
-            A[v2].add(v1);
-        }
-        for(int i = 1; i <= n; i++) {
-            if(!visited[i]){
-                count++;
-                DFS(i);
-            }
-        }
+	static void bfs(int depth) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(depth);
+		visited[depth] = true;
+		
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			for (int i : list[now]) {
+				if (!visited[i]) {
+					queue.offer(i);
+					visited[i] = true;
+				}
+			}
+		}
 
-        System.out.println(count);
-    }
-
-    private static void DFS(int v) {
-        // 한번 방문했으면 return;
-        if (visited[v]) {
-            return;
-        }
-
-        visited[v] = true; // 2를 true로 바꾸고 그 인접한거 확인
-        for (int i : A[v]) { //2->3 4 5 6일때
-            if (!visited[i]) {
-                DFS(i);
-            }
-        }
-    }
+	}
 }
