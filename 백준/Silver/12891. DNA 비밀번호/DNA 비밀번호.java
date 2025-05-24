bd39975f84b1afab84fbdf6ea7c1f2b6d6417c28
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-05-24 11:42:50 +0900

    [Silver II] Title: DNA 비밀번호, Time: 260 ms, Memory: 20064 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,117 +1,91 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-/**
- * 9 8
- * CCTGGATTG
- * 2 0 1 1
- * <p>
- * {‘A’, ‘C’, ‘G’, ‘T’} A: 0, C: 1, G:2, T:3
- */
 public class Main {
-    static int N, M;
-    static int A[]; // 2 0 1 1
-    static int checkSecret = 0;
-    static int answer = 0;
+	static int acgt[];
+	static int pw[];
+	static int answer;
 
-    static int count[] = new int[4];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int len = Integer.parseInt(st.nextToken()); // 5 3
+		int pwlen = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        char[] chars = br.readLine().toCharArray();
-        int start = 0;
-        int end = M - 1;
-        A = new int[4];
-
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < 4; i++) {
-            A[i] = Integer.parseInt(st.nextToken()); // 2 0 1 1
-            
-            if (A[i] == 0) {
-                checkSecret++;
-            }
-        }
-
-        //초기에 값 확인 0부터 부분개수 까지의 값
-        for (int i = 0; i < M; i++) {
-            add(chars[i]);
-        }
-
-        if (checkSecret == 4) answer++;
+		String s = br.readLine().trim(); // GATAG
 
 
-        for (int i = M; i < N; i++) {
-            int j = i - M;
-            // 를 하는데
-            add(chars[i]);
-            remove(chars[j]);
-//            System.out.println("현재 윈도우: " + new String(chars, j + 1, M));
-//            System.out.println("현재 count: " + count[0] + " " + count[1] + " " + count[2] + " " + count[3]);
-//            System.out.println("현재 checkSecret: " + checkSecret);
+		acgt = new int[4];
+		pw = new int[4];
+		st = new StringTokenizer(br.readLine());
+		answer = 0;
 
-            if (checkSecret == 4) {
-                answer++;
-            }
-        }
-        System.out.println(answer);
+		for (int i = 0; i < 4; i++) {
+			acgt[i] = Integer.parseInt(st.nextToken()); // 1 0 0 1
+		}
+		sol(s, pwlen, len);
+		System.out.println(answer);
 
-    }
+	}
 
-    // 알파펫 들어오면 그거에 대한 A[]를 --하고, 값이 0이면 checkSecret ++
-    static private void remove(char c) {
-        switch (c) {
-            case 'A':
-                if (count[0] == A[0]) checkSecret--;
-                count[0]--;
-                break;
+	static void sol(String s, int pwlen, int len) {
+		int arr[] = new int[4];
+		// change
+		// -1 이면 return 0;
 
-            case 'C':
-                if (count[1] == A[1]) checkSecret--;
-                count[1]--;
-                break;
+		// 초기값:
+		for (int i = 0; i < pwlen; i++) {
+			char c = s.charAt(i);
+			int idx = change(c);
 
-            case 'G':
-                if (count[2] == A[2]) checkSecret--;
-                count[2]--;
-                break;
+			if (idx == -1) {
+				
+				return;
+			}
+			arr[idx]++;
+		}
+		
+		if(check(arr))answer++;
 
-            case 'T':
-                if (count[3] == A[3]) checkSecret--;
-                count[3]--;
-                break;
-        }
-    }
+		// 여기부터 슬라이딩
+		for (int i = pwlen; i < len; i++) {
+			
+			int add= change(s.charAt(i));
+			int remove= change(s.charAt(i-pwlen));
 
-    static private void add(char c) {
-        switch (c) {
-            case 'A':
-                count[0]++;
-                if (count[0] == A[0]) checkSecret++;
-                break;
+			arr[add]++;
+			arr[remove]--;
+			
+			if(check(arr)) {
+				answer++;
+			}
+			
+		}
 
-            case 'C':
-                count[1]++;
-                if (count[1] == A[1]) checkSecret++;
-                break;
+	}
+	static boolean check(int[] arr) {
+		for (int i = 0; i < 4; i++) {
+			if(arr[i] < acgt[i]) return false;
+		}
+		return true;
+	}
+//	7 2
+//	GATAAAT
+//	1 0 0 1
 
-            case 'G':
-                count[2]++;
-                if (count[2] == A[2]) checkSecret++;
-                break;
-
-            case 'T':
-                count[3]++;
-                if (count[3] == A[3]) checkSecret++;
-                break;
-        }
-    }
+	// c가 들어오면 인덱스번호로 바꿔주
+	static int change(char c) {
+		int num = -1;
+		if (c == 'A') {
+			num = 0;
+		} else if (c == 'C') {
+			num = 1;
+		} else if (c == 'G') {
+			num = 2;
+		} else if (c == 'T') {
+			num = 3;
+		}
+		return num;
+	}
 }
