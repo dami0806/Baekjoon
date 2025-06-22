7a2e0ca840eb3788ee2a81c3cf9d5de331eb40ad
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-06-22 19:02:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,9 @@
 function solution(rsp) {
-    // 가위 2 바위 0 보 5
-    let arr= {
-        2: 0,
-        0: 5,
-        5: 2
-   
-    };
-    return[...rsp].map(a=>arr[a]).join("")
+    let a = {
+        "2" : "0",
+        "0" : "5",
+        "5" : "2",
+    }
+    
+    return [...rsp].map(i=> a[i]).join("");
 }
