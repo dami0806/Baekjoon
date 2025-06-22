80adf1be4aba6670c29fa95f1cc9bc8b12071e06
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-06-22 19:08:57 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,6 @@
 function solution(numbers) {
-    numbers.sort((a,b) =>a-b)
-    let len =numbers.length;
-    let m1 = numbers[0] * numbers[1];
-    let m2 = numbers[len-1] * numbers[len-2];
-    return m1> m2? m1: m2;
+    //가장 양수 2개, 음수 2개, 가장작은 음수*양수 =>가장 큰 2개랑 가장 작은 2개뽑으면 될듯? 100 -1 -5
+    numbers = numbers.sort((a,b) => a-b);
+    
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length-1] * numbers[numbers.length-2]);
 }
