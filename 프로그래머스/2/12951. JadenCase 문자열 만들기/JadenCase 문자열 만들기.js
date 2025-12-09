98684f482ef1a2c8b40881b22a4a1700578bb10a
dami0806 <85047035+dami0806@users.noreply.github.com>
2025-12-09 23:52:20 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,15 @@
 function solution(s) {
-    return s.split(" ")
-            .map(word => {
-            if(word === "") return "";
-            return word[0].toUpperCase()+ word.slice(1).toLowerCase();
-    }).join(" ");
+    // 문자열 분리
+    const words = s.split(" ")
+    
+    const wordsLower = words.map(x=> {
+        // 첫글자는 대문자
+        if (x === "") return ;
+        const first = x[0].toUpperCase();
+        const rest = x.slice(1).toLowerCase();
+        
+        return first+rest;
+    });
+   
+    return wordsLower.join(" ")
 }
