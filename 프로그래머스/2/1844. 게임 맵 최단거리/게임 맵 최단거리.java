4cd8c85ccb22995eb770bc9ce70b1601505b8800
dami0806 <85047035+dami0806@users.noreply.github.com>
2026-02-21 23:07:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.15 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,41 @@
 import java.util.*;
 class Solution {
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,-1,0,1};
+    static int []dx = {1,0,-1,0};
+    static int []dy ={0,1,0,-1};
     static boolean[][] visited;
-    static int[][] A;
-    static int N;
-    static int M;
-    public int solution(int[][] maps) {
-        int answer = 0;
-        // 최단거리? -> BFS
-        N = maps.length;
-        M = maps[0].length;
-        
-        visited = new boolean[N][M];
-        A = new int[N][M];
-        
-        BFS(0,0,maps);
-        return visited[N-1][M-1]? maps[N-1][M-1] : -1;
-    }
-    static int BFS(int i, int j, int maps[][]) {
-        Queue<int[]> queue = new LinkedList<>();
-        //초기에는 바로 queue에 넣고 방문으로
-        queue.offer(new int[]{i,j});
     
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+
+        return bfs(maps, n, m);
+    }
+    
+    int bfs(int[][] maps, int n,int m) {
+       
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0});
         
-        //지금의 큐 빼고, 그 큐에 대한 숫자 삽입할건데 삽입 전 체크
-            //범위 안에서
-            // 1이고, 방문 안한거
-           while(!queue.isEmpty()) {
-              int now[] = queue.poll();
-               visited[i][j] = true;
-               // 도착지 도착 시 반환
-               if(now[0] == N-1 && now[1] == M-1) {
-                   return maps[now[0]][now[1]];
-               }
-               
-            for(int k = 0;k<4;k++){
-                
-                int x = now[0]+dx[k];
-                int y = now[1]+ dy[k];
-                
-                if(x>=0 && y>=0 && x<N && y<M) {
-                    if(maps[x][y] == 1 && !(visited[x][y])){
-                        maps[x][y] = maps[now[0]][now[1]]+1;
-                        visited[x][y] = true;
-                         queue.offer(new int[]{x,y});
-                    } 
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            if (x == n - 1 && y == m - 1) {
+                return maps[x][y];
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (maps[nx][ny] == 1) { 
+                        maps[nx][ny] = maps[x][y] + 1;
+                        queue.add(new int[]{nx, ny});
+                    }
                 }
             }
-           }
-         return -1;
         }
+        return -1;
+    }
 }
