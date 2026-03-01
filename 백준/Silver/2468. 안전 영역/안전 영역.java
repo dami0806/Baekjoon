8db15497660f91c35b4cbe9dda6b0d2eb87c60bb
dami0806 <85047035+dami0806@users.noreply.github.com>
2026-03-01 16:44:29 +0900

    [Silver I] Title: 안전 영역, Time: 236 ms, Memory: 36484 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,76 +1,88 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int N;
-    static int[][] boardorigin;
-    static int[][] board;
-    static boolean[][] visited;
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
+/**
+ * 
 
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        boardorigin = new int[N][N];
-        board = new int[N][N];
-        visited = new boolean[N][N];
-        int maxWater = 0;// 최대로 찰수 있는 물 높이
-        int count = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        // 보드 만들기
-        for(int i = 0 ; i < N ; i++) {
-            st= new StringTokenizer(br.readLine());
-            for(int j =  0 ; j < N; j ++) {
-                boardorigin[i][j] = Integer.parseInt(st.nextToken());
-                maxWater = Math.max(maxWater, boardorigin[i][j]);
-            }
-        }
+5
+6 8 2 6 2
+3 2 3 4 6
+6 7 3 3 2
+7 2 5 3 6
+8 9 5 2 7
 
-        // 물양에 대한 보드 재 생성
-        for(int i = 0; i<maxWater; i++) { // 물이 1~ maxWater 찼을때
-            for(int m = 0; m < N ; m ++) {
-                for(int n = 0; n < N ; n++) {
-                    board[m][n] = boardorigin[m][n] - i;
-                }
-            }
-            for(int m = 0; m < N ; m++) {
-                for(int n = 0; n < N ; n++) {
-                    if(board[m][n] > 0 && !visited[m][n]) {
-                        dfs(m,n);
-                        count++;
-                    }
-                }
-            }
-            list.add(count);
-            count=0;
-            for(int m = 0; m < N ; m++) {
-                    Arrays.fill(board[m],0);
-                    Arrays.fill(visited[m],false);
-                }
-            }
+ */
+public class Main{
+	static boolean visited[][];
+	static int board[][];
+	static int[] dx = {1,0,-1,0};
+	static int[] dy = {0,1,0,-1};
+	static int N;
 
-        Collections.sort(list, Collections.reverseOrder());
+	static public void main(String args[])throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		int MaxWater = 0;
+		int MaxCount = 0;
 
-        System.out.println(list.get(0));
-    }
-
-    static void dfs(int i,int j) {
-        int nowx = i;
-        int nowy = j;
-        visited[nowx][nowy] = true;
-
-        for(int k = 0 ; k < 4 ;k++){
-            int nx = nowx+dx[k];
-            int ny = nowy+dy[k];
-
-            if(nx >=0 && ny >= 0 && nx <N && ny <N){
-                if(!visited[nx][ny] && board[nx][ny]>0) {
-                    visited[nx][ny] = true;
-                    dfs(nx, ny);
-                }
-            }
-        }
-    }
+		board = new int[N+1][N+1];
+		
+		for(int i= 0 ; i < N ; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j= 0 ; j < N ; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				MaxWater = Math.max(MaxWater, board[i][j]);
+			}
+		}
+		// 1부터 최대수-1까지 
+		for(int water = 0; water< MaxWater; water++) {
+			
+			// 물이 water만큼 찼을때
+			visited = new boolean[N+1][N+1];
+			int count = 0;
+			
+			for(int i= 0 ; i < N ; i++) {
+				for(int j= 0 ; j < N ; j++) {
+					if(!visited[i][j] && board[i][j]-water > 0) {
+						bfs(i,j,water);
+						count++;
+					}
+				}
+			}	
+			MaxCount = Math.max(MaxCount, count);
+		}
+		System.out.println(MaxCount);
+		
+//		for(int i= 0 ; i < N ; i++) {			
+//			for(int j= 0 ; j < N ; j++) {
+//				System.out.print(board[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+	}
+	static void bfs(int i, int j, int water) {
+		// 방문 안하고, 범위 내에 있고,  board[i][j]-water > 0이거면쭉 돌기 
+		visited[i][j] = true;
+		Queue<int[]> queue = new ArrayDeque();
+		queue.add(new int[] {i,j});
+		
+		while(!queue.isEmpty()) {
+			int cur[] = queue.poll();
+			for(int k = 0 ; k< 4 ; k++) {
+				int nx = cur[0]+dx[k];
+				int ny = cur[1]+dy[k];
+				
+				if(nx >=0 && ny >= 0 && nx <N && ny<N) {
+					if (!visited[nx][ny] && board[nx][ny]-water>0) {
+						visited[nx][ny] = true;
+						queue.add(new int[] {nx, ny});
+					}
+				}
+			}
+		}
+	}
 }
+	
